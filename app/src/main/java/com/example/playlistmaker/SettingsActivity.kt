1ac91bcc2e814a56d4896b57bb301fe97d856d09
KateSilencio <ekate.austr89@gmail.com>
2024-07-24 22:40:59 +0300

    Спринт 12 Хранение данных SharedPreferences Версия 1

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -6,8 +6,11 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.ImageButton
 import android.widget.LinearLayout
+import com.google.android.material.switchmaterial.SwitchMaterial
+
+const val SWITCH_PREFERENCES = "practicum_example_preferences"
+const val SWITCH_IS_CHECKED = "switch_is_checked"
 
 class SettingsActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId", "WrongViewCast")
@@ -15,16 +18,14 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        //Активация тулбара для окна Настройки
         val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_settings)
         if (toolbar != null) {
             setSupportActionBar(toolbar)
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        /*val backwardBtn = findViewById<ImageButton>(R.id.backward_settings_btn)
-        backwardBtn.setOnClickListener {
-            finish()
-        }*/
 
+        //интент для отправки ссылки с выбором приложения
         val layoutShare = findViewById<LinearLayout>(R.id.LayoutShare)
         layoutShare.setOnClickListener{
             val shareIntent = Intent(Intent.ACTION_SEND).apply {
@@ -37,6 +38,7 @@ class SettingsActivity : AppCompatActivity() {
             }
         }
 
+        //интент для отправки сообщения по электронной почте
         val layoutSupport = findViewById<LinearLayout>(R.id.LayoutSupport)
         layoutSupport.setOnClickListener{
             Intent(Intent.ACTION_SENDTO).apply {
@@ -48,6 +50,7 @@ class SettingsActivity : AppCompatActivity() {
             }
         }
 
+        //интент для запуска браузера с веб страницей
         val layoutAgreement = findViewById<LinearLayout>(R.id.LayoutAgreement)
         layoutAgreement.setOnClickListener{
             val site = Uri.parse(getString(R.string.agreement_link))
@@ -55,6 +58,17 @@ class SettingsActivity : AppCompatActivity() {
             if (agreementIntent.resolveActivity(packageManager)!=null){
                 startActivity(agreementIntent)
             }
+        }
+
+        val sharedPrefs = getSharedPreferences(SWITCH_PREFERENCES, MODE_PRIVATE)
+
+        val themeSwitcher = findViewById<SwitchMaterial>(R.id.switch_theme)
+        //Переключатель темы + сохранение
+        themeSwitcher.setOnCheckedChangeListener { switcher, checked ->
+            (applicationContext as App).switchTheme(checked)
+            sharedPrefs.edit()
+                .putBoolean(SWITCH_IS_CHECKED,checked)
+                .apply()
         }
     }
     //Обработка кнопки Назад
